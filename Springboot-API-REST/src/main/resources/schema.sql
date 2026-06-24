a8ff90a1514e5107effcf9fa867c192e594af7e7
unknown <osvaldofuentesr>
2026-06-23 21:11:14 -0400

    AGREGAR DATA.SQL Y SCHEMA.SQL

--- Springboot-API-REST/src/main/resources/schema.sql
+++ Springboot-API-REST/src/main/resources/schema.sql
@@ -1,11 +1,8 @@
-INSERT INTO venta (id_venta, direccion_compra, valor_compra, fecha_compra, despacho_generado) 
-VALUES (1, 'P Sherman Calle Wallabi 42 Syndey', 22990, '2024-02-02', false);
-
-INSERT INTO venta (id_venta, direccion_compra, valor_compra, fecha_compra, despacho_generado) 
-VALUES (2, 'Avenida siempre viva 69', 12590, '2024-03-05', false);
-
-INSERT INTO venta (id_venta, direccion_compra, valor_compra, fecha_compra, despacho_generado) 
-VALUES (3, 'Avenida Por atrás 1313', 13990, '2024-04-20', false);
-
-INSERT INTO venta (id_venta, direccion_compra, valor_compra, fecha_compra, despacho_generado) 
-VALUES (4, 'Calle presidente kirby 8528', 9990, '2024-04-15', false);
+-- schema.sql
+CREATE TABLE IF NOT EXISTS venta (
+    id_venta BIGINT PRIMARY KEY,
+    direccion_compra VARCHAR(255),
+    valor_compra DOUBLE,
+    fecha_compra DATE,
+    despacho_generado BOOLEAN
+);
